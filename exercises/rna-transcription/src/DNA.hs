41edfb6767dc34d371fe94c28be2084d30898872
Simon Shine <shreddedglory@gmail.com>
2019-12-28 02:45:54 +0100

    rna-transcription

---
+++ exercises/rna-transcription/src/DNA.hs
@@ -0,0 +1,11 @@
+module DNA (toRNA) where
+
+toRNA :: String -> Either Char String
+toRNA = traverse fromDNA
+  where
+    fromDNA :: Char -> Either Char Char
+    fromDNA 'G' = pure 'C'
+    fromDNA 'C' = pure 'G'
+    fromDNA 'T' = pure 'A'
+    fromDNA 'A' = pure 'U'
+    fromDNA c   = Left c
