ba24a8fe909b6af1675dd671141ec89e7efcb0ed
Simon Shine <shreddedglory@gmail.com>
2019-12-28 04:18:54 +0100

    leap: Replace with Data.Time.Calendar.isLeapYear

--- exercises/leap/src/LeapYear.hs
+++ exercises/leap/src/LeapYear.hs
@@ -1,5 +1,3 @@
 module LeapYear (isLeapYear) where
 
-isLeapYear :: Integer -> Bool
-isLeapYear year =
-  year `rem` 4 == 0 && (year `rem` 100 /= 0 || year `rem` 400 == 0)
+import Data.Time.Calendar (isLeapYear)
