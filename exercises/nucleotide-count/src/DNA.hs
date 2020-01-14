8e21602228fe0052204e144904541940902923a5
Simon Shine <shreddedglory@gmail.com>
2020-01-14 01:22:36 +0100

    nucleotide-count: Restructure slightly

--- exercises/nucleotide-count/src/DNA.hs
+++ exercises/nucleotide-count/src/DNA.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import qualified Data.Map as Map
@@ -8,16 +9,13 @@ data Nucleotide = A | C | G | T
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts = fmap (pad . frequencies) . traverse toNucleotide
+nucleotideCounts = fmap frequencies . traverse toNucleotide
 
 toNucleotide :: Char -> Either String Nucleotide
-toNucleotide c = readEither [c] <> Left ("Unknown nucleotide " ++ show c)
+toNucleotide c = readEither [c] <> Left [c]
 
 frequencies :: [Nucleotide] -> Map Nucleotide Int
-frequencies = Map.fromListWith (+) . (`zip` [1,1..])
+frequencies = (`Map.union` zeroMap) . Map.fromListWith (+) . (`zip` [1,1..])
 
-pad :: Map Nucleotide Int -> Map Nucleotide Int
-pad = (`Map.union` emptyMap)
-
-emptyMap :: Map Nucleotide Int
-emptyMap = Map.fromListWith (+) (zip [minBound..] [0,0..])
+zeroMap :: Map Nucleotide Int
+zeroMap = Map.fromList $ map (,0) [minBound..]
